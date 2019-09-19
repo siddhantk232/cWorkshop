891b9d1cb60af28a730de89096239463ea2f7083
Siddhant Kumar <siddhantk232@gmail.com>
2019-09-19 00:50:11 -0700

    added day02.c

---
+++ day02.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+// int main () {
+// 	int a;
+
+// 	printf("enter a number\n");
+// 	scanf("%d", &a);
+
+// 	if (a % 2 == 0) {
+// 		printf("the number is even");
+// 	} else {
+// 		printf("the number is odd");
+// 	}
+// }
+
+// int main () {
+// 	int a;
+
+// 	printf("give me your score\n");
+// 	scanf("%d", &a);
+
+// 	if ( a > 49 && a < 100) {
+// 		printf("You are the man! just did a half century");
+// 	} else {
+// 		printf("well played");
+// 	}
+// }
+
+// int main () {
+// 	int a;
+
+// 	printf("enter a number\n");
+// 	scanf("%d", &a);
+
+// 	if (a % 7 == 0 && a > 15) {
+// 		printf("true");
+// 	} else {
+// 		printf("false");
+// 	}
+// }
+
+// int main () {
+// 	int a,b;
+
+// 	printf("enter two numbers\n");
+// 	scanf("%d %d", &a, &b);
+
+// 	if (a > b) {
+// 		printf("a - b = %d", a - b);
+// 	} else {
+// 		printf("a + b = %d", a + b);
+// 	}
+// }
+
+int main () {
+	int a,b;
+
+	printf("enter two numbers\n");
+	scanf("%d %d", &a, &b);
+
+ 	if (a = >b) {
+ 		printf("a^b = %d", a ^ b);
+ 	} else {
+ 		printf("%d %d", a , b);
+ 	}
+
+}
